d6c6665ef198e139f77f3dfcfeadb06462e9b088
tmo <tmo@mtb111.fr>
2023-05-31 17:12:19 +0200

    try to build a live bot. Need new design

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,68 @@
 use std::sync::atomic::AtomicBool;
+use binance::api::Binance;
+use binance::general::General;
+use binance::market::Market;
 use binance::websockets::{WebsocketEvent, WebSockets};
 use strategy_backtester::backtest::*;
 use strategy_backtester::patterns::*;
 use strategy_backtester::strategies::*;
 use strategy_backtester::strategies_creator::*;
 use strategy_backtester::*;
+use strategy_backtester::tools::retreive_test_data;
 
 const START_MONEY: f64 = 100.;
+const DATA_FOLDER: &str = "data/";
 
 fn main() {
-    //let market: Market = Binance::new(None, None);
+    /*//let market: Market = Binance::new(None, None);
     //let general: FuturesGeneral = Binance::new(None, None);
-    let keep_running = AtomicBool::new(true); // Used to control the event loop
-    /*let endpoints = ["kline_1m"]
-        .map(|symbol| format!("btcusdt@{}", symbol.to_lowercase()));*/
-    let kline_endpoint = format!("{}", "btcusdt@kline_1m");
-    let mut kline_web_socket = WebSockets::new(|event: WebsocketEvent| {
-        match event {
-            WebsocketEvent::Kline(kline_event) => {
-                println!("Symbol: {}, High: {}, Low {}", kline_event.symbol, kline_event.kline.high, kline_event.kline.low);
-            }
-            _ => (),
-        };
-        Ok(())
-    });   
+    let keep_running = AtomicBool::new(true);*/ // Used to control the event loop
 
-    kline_web_socket.connect(&kline_endpoint).unwrap(); // check error
-    if let Err(e) = kline_web_socket.event_loop(&keep_running) {
-        match e {
-          err => {
-             println!("Error: {:?}", err);
-          }
+    let market: Market = Binance::new(None, None);
+    let general: General = Binance::new(None, None);
+    let endpoints = ["btcusdt@trade".to_string(), "btcusdt@kline_1m".to_string(), "ethusdt@trade".to_string(), "ethusdt@kline_1m".to_string()];
+
+    let mut server_time = 0;
+    let result = general.get_server_time();
+    match result {
+        Ok(answer) => {
+            println!("Server Time: {}", answer.server_time);
+            server_time = answer.server_time;
         }
+        Err(e) => println!("Error: {}", e),
     }
-    kline_web_socket.disconnect().unwrap();
+    let keep_running = AtomicBool::new(true);
 
 
+    let symbol = "BTCUSDT".to_string();
+    let interval = "1m".to_string();
+    let klines = retreive_test_data(server_time, &market, symbol, interval, DATA_FOLDER.to_string(), 1, 25);
+    let readable_klines = Backtester::to_all_math_kline(klines);
+    let arc_klines = Arc::new(readable_klines);
 
-    let trades_endpoint = format!("{}", "btcusdt@trades");
-    let mut trades_web_socket = WebSockets::new(|event: WebsocketEvent| {
+
+    let results = Backtester::new(arc_klines, None, None)
+    .add_strategies(&mut arc_klines)
+    .start()
+    .get_results();
+
+    let mut web_socket = WebSockets::new(|event: WebsocketEvent| {
         match event {
             WebsocketEvent::Trade(trade_event) => {
                 println!("Symbol: {}, Price: {}", trade_event.symbol, trade_event.price);
+                // Ici mettre a jour la variable de prix
+                // genre current_price = trade_event.price;
             },
             _ => (),
         };
         Ok(())
-    });   
+    });
 
-    trades_web_socket.connect(&trades_endpoint).unwrap(); // check error
-    if let Err(e) = trades_web_socket.event_loop(&keep_running) {
-        match e {
-          err => {
-             println!("Error: {:?}", err);
-          }
-        }
+    web_socket.connect_multiple_streams(&endpoints).unwrap(); // check error
+    if let Err(e) = web_socket.event_loop(&keep_running) {
+        println!("Error: {:?}", e);
     }
-    trades_web_socket.disconnect().unwrap();
-
-
-
+    web_socket.disconnect().unwrap();
     /*let strategies = create_w_and_m_pattern_strategies(
         START_MONEY,
         ParamMultiplier {
